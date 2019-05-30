d433dca52da26f1518dcacaa43bdcf2a83790fe9
fieldtianye.my <fieldtianye.my@163.com>
2019-05-30 15:18:37 +0800

    修改composer.json

--- _mine_study/time_mine.php
+++ _mine_study/time_mine.php
@@ -6,9 +6,27 @@
  * Time: 下午3:10
  */
 
+require_once '_common.php';
+
 
 // date_default_timezone_set('UTC');
 $start_time = date('Y-m-d H:i:s', time());
 println(date("Y-m-d"));
 println($start_time);
 println(strtotime('now'));
+
+
+function heartbeat()
+{
+    function m_sectime()
+    {
+        list($msec, $sec) = explode(' ', microtime());
+        return (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);
+        // return (int)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);
+    }
+
+    $m_sectime = m_sectime();
+    echo $m_sectime . "\n";
+}
+
+heartbeat();
